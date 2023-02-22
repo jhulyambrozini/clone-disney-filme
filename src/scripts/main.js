cecf1d8370a68c5dda17016968ffe05767717a52
jhulyamfebr@gmail.com <jhulyambrozini@gmail.com>
2023-02-22 02:06:21 -0300

    feat: finalizando layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,47 @@
-console.log('ola mundo')
+document.addEventListener('DOMContentLoaded', function() {
+
+    const aboutSection = document.querySelector('.desc__sinopse')
+    const aboutHeight = aboutSection.clientHeight;
+
+    const descSection = document.querySelector('.desc')
+    const descHeight = descSection.clientHeight;
+
+
+    window.addEventListener('scroll', function() {
+        const position = window.scrollY;
+
+        if (position <= aboutHeight) {
+            hiddenHeaderElements();
+
+        } else {
+            showHeaderElements();
+        }
+
+        if (position <= descHeight) {
+            hiddenOpacity()
+        } else {
+            showOpacity()
+        }
+    })
+
+})
+
+function showOpacity() {
+    const background = document.querySelector('.app__background')
+    background.classList.add('opacity')
+}
+
+function hiddenOpacity() {
+    const background = document.querySelector('.app__background')
+    background.classList.remove('opacity')
+}
+
+function hiddenHeaderElements() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function showHeaderElements() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
